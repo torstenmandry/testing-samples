42c8c8c75e65545fa6716831045e0c42d4ff63cf
Torsten Mandry <torsten.mandry@opitz-consulting.com>
2013-05-30 12:28:44 +0200

    hamcrest matchers

--- src/test/java/de/javandry/testingsamples/junit/InvestmentArmortisationTest.java
+++ src/test/java/de/javandry/testingsamples/junit/InvestmentArmortisationTest.java
@@ -11,8 +11,11 @@ import java.math.BigDecimal;
 import java.util.Calendar;
 import java.util.GregorianCalendar;
 
+import static de.javandry.testingsamples.matcher.PositionValueMatcher.containsMonthsFromTo;
+import static de.javandry.testingsamples.matcher.PositionValueMatcher.haveConstantAmount;
 import static java.lang.String.format;
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertThat;
 
 public class InvestmentArmortisationTest {
 
@@ -70,16 +73,8 @@ public class InvestmentArmortisationTest {
 
     // dann folgt daraus eine monatliche Abschreibung in Höhe von 10 TEUR
     // in der Zeit von 01/2012 bis 12/2016
-    assertEquals("number of armortisation values", 60, investment.getArmortisation().size());
-    for (int i = 0; i < 60; i++) {
-      PositionValue value = investment.getArmortisation().get(i);
-
-      Calendar expectedMonthYear = new GregorianCalendar(2012, 1 - 1, 1);
-      expectedMonthYear.add(Calendar.MONTH, i);
-
-      assertEquals(format("value %d - month", i), expectedMonthYear.get(Calendar.MONTH) + 1, value.getMonth());
-      assertEquals(format("value %d - year", i), expectedMonthYear.get(Calendar.YEAR), value.getYear());
-      assertEquals(format("value %d - amount", i), BigDecimal.valueOf(10000d), value.getAmount());
-    }
+    assertThat(investment.getArmortisation(), haveConstantAmount(10000));
+    assertThat(investment.getArmortisation(), containsMonthsFromTo(1, 2012, 12, 2016));
   }
+
 }
---
+++ src/test/java/de/javandry/testingsamples/matcher/PositionValueMatcher.java
@@ -0,0 +1,88 @@
+package de.javandry.testingsamples.matcher;
+
+import de.javandry.testingsamples.app.PositionValue;
+import org.hamcrest.Description;
+import org.hamcrest.Matcher;
+import org.hamcrest.TypeSafeMatcher;
+
+import java.math.BigDecimal;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
+import java.util.List;
+
+import static java.lang.String.format;
+
+public class PositionValueMatcher {
+
+  public static Matcher<? super List<PositionValue>> haveConstantAmount(final double expectedAmount) {
+    return new TypeSafeMatcher<List<PositionValue>>() {
+      private PositionValue currentPositionValue;
+
+      @Override
+      protected boolean matchesSafely(List<PositionValue> positionValues) {
+        for (PositionValue value : positionValues) {
+          currentPositionValue = value;
+          if (!currentPositionValue.getAmount().equals(BigDecimal.valueOf(expectedAmount))) {
+            return false;
+          }
+        }
+        return true;
+      }
+
+      @Override
+      public void describeTo(Description description) {
+        description.appendText(format("unexpected amount in %02d/%4d: %.2f",
+            currentPositionValue.getMonth(),
+            currentPositionValue.getYear(),
+            currentPositionValue.getAmount()));
+      }
+    };
+  }
+
+  public static Matcher<? super List<PositionValue>> containsValueFor(final int month, final int year) {
+    return new TypeSafeMatcher<List<PositionValue>>() {
+      @Override
+      protected boolean matchesSafely(List<PositionValue> positionValues) {
+        for (PositionValue nextValue : positionValues) {
+          if (nextValue.getMonth() == month && nextValue.getYear() == year) {
+            return true;
+          }
+        }
+        return false;
+      }
+
+      @Override
+      public void describeTo(Description description) {
+        description.appendText(format("does not contain value for %2d/%4d", month, year));
+      }
+    };
+  }
+
+  public static Matcher<? super List<PositionValue>> containsMonthsFromTo(final int fromMonth, final int fromYear,
+                                                                          final int toMonth, final int toYear) {
+    return new TypeSafeMatcher<List<PositionValue>>() {
+      private Calendar currentMonthYear;
+
+      @Override
+      protected boolean matchesSafely(List<PositionValue> positionValues) {
+        Calendar fromMonthYear = new GregorianCalendar(fromYear, fromMonth - 1, 1);
+        Calendar toMonthYear = new GregorianCalendar(toYear, toMonth - 1, 2); // set second day so that we can safely
+        // use before method, even for the last month
+        for (currentMonthYear = fromMonthYear; currentMonthYear.before(toMonthYear); currentMonthYear.add(Calendar
+            .MONTH, 1)) {
+          if (!containsValueFor(currentMonthYear.get(Calendar.MONTH) + 1, currentMonthYear.get(Calendar.YEAR))
+              .matches(positionValues)) {
+            return false;
+          }
+        }
+        return true;
+      }
+
+      @Override
+      public void describeTo(Description description) {
+        description.appendText(format("does not contain value for %2d/%4d", currentMonthYear.get(Calendar.MONTH) + 1, currentMonthYear.get(Calendar.YEAR)));
+      }
+    };
+  }
+
+}
